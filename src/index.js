a054d3c88b0db79eeec5c545e9123ad4dd8a5818
merimiMehdi <mhidomer@gmail.com>
2022-12-07 20:42:41 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import style from './components/css/style.css';
+import Site from './components/Site';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Site />
+  </React.StrictMode>
+);
+
